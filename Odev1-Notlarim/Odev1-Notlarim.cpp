5208fd4e16e86142df288355be0bf7bf125bab26
efetiryakioglu <efetiryakioglu1@gmail.com>
2023-01-19 14:09:01 +0300

    Proje dosyası ekle.

---
+++ Odev1-Notlarim/Odev1-Notlarim.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	int s, i;
+	s = 0;
+	i = 1;
+a:	s = s + i;
+	i = i + 1;
+	cout << " " << s << endl;
+	if (i <= 100) goto a;
+	cout << "s= " << s;
+}
